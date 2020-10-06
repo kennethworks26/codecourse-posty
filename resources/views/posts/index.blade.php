06921775da290897a1be0d319669b4d91814cc21
Kenneth Fernandez <kennethworks26@gmail.com>
2020-10-07 00:53:24 +0800

    feat(post): pagination

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -35,6 +35,7 @@
                     <p class="mb-2">{{ $post->body }}</p>
                   </div>
                 @endforeach
+                {{ $posts->links() }}
               @else
               <p>There are no posts.</p>
               @endif
