d9f2709f43aa2294f8296592e0fbec444b261640
Kenneth Fernandez <kennethworks26@gmail.com>
2020-10-08 00:42:55 +0800

    feat(post): eager loading

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,7 +14,7 @@ class PostController extends Controller
 
     public function index()
     {
-        $posts = Post::latest()->paginate(10);
+        $posts = Post::with('user')->latest()->paginate(10);
 
         return view('posts.index', [
             'posts' => $posts
